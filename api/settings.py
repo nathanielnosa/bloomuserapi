e87073899f4244460c13761b792a48fc135c29ac
Nathaniel Nosa <nathanielnarth@gmail.com>
2026-02-13 13:16:08 +0100

    update db

--- api/settings.py
+++ api/settings.py
@@ -68,23 +68,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': str(os.getenv('db_name')),
-       'USER': 'postgres',
-       'PASSWORD': str(os.getenv('db_pwd')),
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': str(os.getenv('db_name')),
+#        'USER': 'postgres',
+#        'PASSWORD': str(os.getenv('db_pwd')),
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
